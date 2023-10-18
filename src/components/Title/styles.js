5a85fcbdca7e8d4282d24766962c31cbbdafb921
Eren Tasci <erent4sci@gmail.com>
2023-10-18 23:59:11 +0300

    feat: title, category component

---
+++ src/components/Title/styles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  title: {
+    color: "#000000",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
+
+export default styles;
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -2,12 +2,20 @@ import React from "react";
 import { Text, SafeAreaView } from "react-native";
 import styles from "./styles";
 import Input from "../../components/Input";
+import Title from "../../components/Title";
+import Categories from "../../components/Categories";
 
 const Home = ({ navigation }) => {
   return (
     <SafeAreaView style={styles.container}>
       {/* <Text onPress={() => navigation.navigate("Search")}>Search </Text> */}
       <Input pressable={true} onPress={() => navigation.navigate("Search")} />
+      <Title text="Featured Recipes" />
+      <Categories
+        categories={["All", "Trending"]}
+        selectedCategory="All"
+        onCategoryPress={() => {}}
+      />
     </SafeAreaView>
   );
 };
